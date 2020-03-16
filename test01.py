a212137327e534b2f643a32a5fb0ba3ccb76ca5d
ZiLong <309803073@qq.com>
2020-03-17 01:55:40 +0800

    添加了python解释器 创建了test01.py 创建了test02.py
    Signed-off-by: ZiLong <309803073@qq.com>

---
+++ test01.py
@@ -0,0 +1,11 @@
+# ===============================
+# Author:柠檬班-紫龙
+# Time  :2020/3/17 1:32
+# Email :309803073@qq.com
+# File  :test01.py
+# ===============================
+
+print('adfadfasdf')
+print('nihao nihao')
+
+
